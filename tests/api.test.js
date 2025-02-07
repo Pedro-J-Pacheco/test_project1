36a0fa616d67bb6028eb4af8628dfc31b1397b58
Pedro-J-Pacheco <pjpachecom@gmail.com>
2025-02-06 19:22:55 -0700

    commit update 4

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,6 +8,6 @@ describe ('fecthdata',()=>{
         //Assert
         expect(data).toEqual({id:1,product:"A"})
         expect(fetchData).toHaveBeenCalled();
-//test 121223424
+//test 121223424another number 
     });
 });
