3953de7df563091830f46ebc6ff508e919a42f62
Pedro-J-Pacheco <pjpachecom@gmail.com>
2025-02-11 18:39:38 -0700

    commit cypress 7

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,12 +1,22 @@
-describe ('fecthdata',()=>{
-    it ('should give us data in json', async()=>{
-        const fetchData =jest.fn()
-        //arange
-        fetchData.mockResolvedValue({id:1,product:"A"});
-        //Act
-        const data = await fetchData();
-        //Assert
-        expect(data).toEqual({id:1,product:"A"})
-        expect(fetchData).toHaveBeenCalled();
-//test 121223424another number 2333 344 3423 432432
+const { fetchData } = require('../functions/api.js');
+
+
+
+describe('fetchData', () =>{
+
+        it('should give us data in json', async () =>{
+
+            //1st step is mock the function
+            const fetchData = jest.fn()
+
+            //Arrange.  Mock the function's response
+            fetchData.mockResolvedValue({id: 1, product: "A"});
+
+            //Act by invoking the real function
+            const data = await fetchData();
+
+            //Assert
+            expect(data).toEqual({id: 1, product: "A"})
+            expect(fetchData).toHaveBeenCalled();
+        });       
 });
